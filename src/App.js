ede3656ae8796ef7631c12e47df0d8e6e65a0498
Shashwat Shukla <65778674+Shashwatshukla-88@users.noreply.github.com>
2024-09-07 15:38:46 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './ganesh-5998483_640.jpg';
 import './App.css';
 
 function App() {
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ...Ganpati Bappa Maurya...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
